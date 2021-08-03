b9f9f4cb5b405eeb4223413cd923bb4c3d8f70e1
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-04 00:33:09 +0900

    [#10] feat: 토큰 생성하는 기능 추가

--- src/main/java/com/itda/apiserver/jwt/JwtTokenProvider.java
+++ src/main/java/com/itda/apiserver/jwt/JwtTokenProvider.java
@@ -1,8 +1,12 @@
 package com.itda.apiserver.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Base64;
+import java.util.Date;
 
 public class JwtTokenProvider {
 
@@ -14,5 +18,18 @@ public class JwtTokenProvider {
         this.validityInMilliseconds = validityInSeconds * 1000;
     }
 
+    public String createToken(String subject) {
+        Claims claims = Jwts.claims().setSubject(subject);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
 
 }
