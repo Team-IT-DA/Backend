9e934fde3f246ce74ac194b2cc9ee3ab99524e02
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-05 15:51:04 +0900

    [#11] test: 이메일 중복 확인 기능 테스트 추가
    중복된 이메일이 없는 경우

--- src/test/java/com/itda/apiserver/service/UserServiceTest.java
+++ src/test/java/com/itda/apiserver/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.itda.apiserver.service;
 
 import com.itda.apiserver.domain.User;
+import com.itda.apiserver.dto.EmailVerificationRequestDto;
 import com.itda.apiserver.dto.SignUpRequestDto;
 import com.itda.apiserver.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -27,6 +30,9 @@ public class UserServiceTest {
     @MockBean
     private User user;
 
+    @MockBean
+    private EmailVerificationRequestDto emailRequestDto;
+
     @Test
     @DisplayName("회원 가입 기능 테스트")
     void signUp() {
@@ -36,4 +42,17 @@ public class UserServiceTest {
 
         verify(userRepository, times(1)).save(any(User.class));
     }
+
+    @Test
+    @DisplayName("이메일 중복 확인 기능 테스트, 중복된 이메일이 없는 경우")
+    void verifyEmail() {
+        when(emailRequestDto.getEmail()).thenReturn("yeon@gmail.com");
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        userService.verifyEmail(emailRequestDto);
+
+        verify(emailRequestDto, times(1)).getEmail();
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
 }
