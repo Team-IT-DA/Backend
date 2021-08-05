c73d639bdd9f185c2f731be2bbbd312b8e1321ac
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-05 15:08:01 +0900

    [#11] test: 불필요한 어노테이션 제거

--- src/test/java/com/itda/apiserver/service/UserServiceTest.java
+++ src/test/java/com/itda/apiserver/service/UserServiceTest.java
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-@Transactional
 public class UserServiceTest {
 
     @Autowired
