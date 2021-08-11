d9b0586b5d96a10b132bda5b5de448504d348d49
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-11 15:03:29 +0900

    [#10] feat: AuthInterceptor 추가
    LoginRequired 어노테이션이 있는지 확인하고, 있다면 token에서 user id값을 추출, request에 추출한 user id를 추가하는 역할

---
+++ src/main/java/com/itda/apiserver/interceptor/AuthInterceptor.java
@@ -0,0 +1,45 @@
+package com.itda.apiserver.interceptor;
+
+import com.itda.apiserver.annotation.LoginRequired;
+import com.itda.apiserver.jwt.TokenExtractor;
+import com.itda.apiserver.jwt.TokenProvider;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final String JWT_HEADER = "Authorization";
+
+    private final TokenProvider tokenProvider;
+    private final TokenExtractor tokenExtractor;
+
+    public AuthInterceptor(TokenProvider tokenProvider, TokenExtractor tokenExtractor) {
+        this.tokenProvider = tokenProvider;
+        this.tokenExtractor = tokenExtractor;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (loginRequired(handler)) {
+            setUserIdOn(request);
+        }
+        return true;
+    }
+
+    private boolean loginRequired(Object handler) {
+        return handler instanceof HandlerMethod
+                && ((HandlerMethod) handler).hasMethodAnnotation(LoginRequired.class);
+    }
+
+    private void setUserIdOn(HttpServletRequest request) {
+        String header = request.getHeader(JWT_HEADER);
+        String token = tokenExtractor.extractToken(header);
+        Long userId = tokenProvider.getUserId(token);
+        request.setAttribute("user_id", userId);
+    }
+}
