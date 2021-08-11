e65ce8311e00d04253d7917fbae58ac56015e28e
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-11 15:28:45 +0900

    [#10] feat: 생성한 UserArgumentResolver 등록

--- src/main/java/com/itda/apiserver/config/WebMvcConfig.java
+++ src/main/java/com/itda/apiserver/config/WebMvcConfig.java
@@ -1,19 +1,29 @@
 package com.itda.apiserver.config;
 
 import com.itda.apiserver.interceptor.AuthInterceptor;
+import com.itda.apiserver.resolver.UserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final UserArgumentResolver userArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);
     }
 }
