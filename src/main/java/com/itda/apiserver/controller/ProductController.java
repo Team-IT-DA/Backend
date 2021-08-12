88bf5f4896d2e2909b006e86c19fb8fbd005eab9
roach <tmdgusya@naver.com>
2021-08-12 23:20:07 +0900

    feat: @LoginRequired 및 @UserId 적용

--- src/main/java/com/itda/apiserver/controller/ProductController.java
+++ src/main/java/com/itda/apiserver/controller/ProductController.java
@@ -15,14 +15,9 @@ public class ProductController {
 
     private final ProductService productService;
 
-    /**
-     * E2E Testing 을 위해서 현재는 Long userId 를 붙여둠
-     * 추후 인터셉터를 통해서 이를 교체할 예정
-     * @param addproductRequestDto
-     * @param userId
-     */
     @PostMapping
-    public ApiResult<Void> addProduct(@RequestBody AddproductRequestDto addproductRequestDto, @RequestParam Long userId) {
+    @LoginRequired
+    public ApiResult<Void> addProduct(@RequestBody AddproductRequestDto addproductRequestDto, @UserId Long userId) {
         productService.addProduct(addproductRequestDto, userId);
         return ApiResult.ok(null);
     }
