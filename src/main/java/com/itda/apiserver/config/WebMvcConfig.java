f9d370c6117f06923c54610fa69997cdc2a5d048
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-19 17:47:07 +0900

    [#5] feat: webClient 빈 등록

--- src/main/java/com/itda/apiserver/config/WebMvcConfig.java
+++ src/main/java/com/itda/apiserver/config/WebMvcConfig.java
@@ -2,13 +2,22 @@ package com.itda.apiserver.config;
 
 import com.itda.apiserver.interceptor.AuthInterceptor;
 import com.itda.apiserver.resolver.UserArgumentResolver;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +25,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
     private final UserArgumentResolver userArgumentResolver;
+
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(connection ->
+                        connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MICROSECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MICROSECONDS)));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
