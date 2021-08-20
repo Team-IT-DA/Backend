dca4250040d404dd7bb00bf8853391938370c02e
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-20 18:29:21 +0900

    [#5] test: 컨트롤러의 네이버로 소셜 로그인하는 기능 테스트 추가

---
+++ src/test/java/com/itda/apiserver/oauth/OauthControllerTest.java
@@ -0,0 +1,43 @@
+package com.itda.apiserver.oauth;
+
+import com.itda.apiserver.dto.TokenResponseDto;
+import com.itda.apiserver.jwt.TokenExtractor;
+import com.itda.apiserver.jwt.TokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OauthController.class)
+@Import(value = {TokenProvider.class, TokenExtractor.class})
+public class OauthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LoginService loginService;
+
+    @Test
+    @DisplayName("네이버로 소셜로그인 하는 기능 테스트")
+    void neverLogin() throws Exception {
+
+        when(loginService.socialLogin(anyString())).thenReturn(new TokenResponseDto("thisIsToken"));
+
+        mockMvc.perform(get("/api/login/naver")
+                .param("code", "authorizationCode"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.token").exists())
+                .andDo(print());
+    }
+}
