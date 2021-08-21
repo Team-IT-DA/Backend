f65038f50411e9835a6780226c5503f2f99a2d27
roach <tmdgusya@naver.com>
2021-08-21 16:17:17 +0900

    feat: 장바구니 key 를 userId 로 변경

--- src/main/java/com/itda/apiserver/repository/ShopBascketRepository.java
+++ src/main/java/com/itda/apiserver/repository/ShopBascketRepository.java
@@ -1,9 +1,9 @@
 package com.itda.apiserver.repository;
 
-import com.itda.apiserver.redis.ShopBascket;
+import com.itda.apiserver.redis.ShopBasket;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShopBascketRepository extends CrudRepository<ShopBascket, String> {
+public interface ShopBascketRepository extends CrudRepository<ShopBasket, Long> {
 }
