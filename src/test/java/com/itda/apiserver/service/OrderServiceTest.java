cd4358775a9b0fbab356bba1567e99924d519295
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-08-31 21:19:55 +0900

    [#31] test: OrderService의 주문 기능 테스트 추가

---
+++ src/test/java/com/itda/apiserver/service/OrderServiceTest.java
@@ -0,0 +1,74 @@
+package com.itda.apiserver.service;
+
+import com.itda.apiserver.domain.OrderHistory;
+import com.itda.apiserver.domain.OrderSheet;
+import com.itda.apiserver.domain.User;
+import com.itda.apiserver.dto.OrderProductRequest;
+import com.itda.apiserver.dto.OrderRequestDto;
+import com.itda.apiserver.dto.ShippingInfoDto;
+import com.itda.apiserver.repository.OrderHistoryRepository;
+import com.itda.apiserver.repository.OrderSheetRepository;
+import com.itda.apiserver.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private OrderHistoryRepository orderHistoryRepository;
+
+    @MockBean
+    private OrderSheetRepository orderSheetRepository;
+
+    @MockBean
+    private OrderValidationService orderValidationService;
+
+    @Mock
+    private User user;
+
+    @Mock
+    private OrderRequestDto orderRequest;
+
+    @Mock
+    private ShippingInfoDto shippingInfoRequest;
+
+    @Mock
+    private List<OrderProductRequest> orderProductList;
+
+    @Test
+    @DisplayName("주문 기능 테스트")
+    void order() {
+
+        when(orderValidationService.isDuplicatedOrder(anyLong())).thenReturn(false);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(orderRequest.getShippingAddress()).thenReturn(shippingInfoRequest);
+        when(orderRequest.getOrderList()).thenReturn(orderProductList);
+
+        orderService.order(1L, orderRequest);
+
+        verify(orderValidationService, times(1)).isDuplicatedOrder(anyLong());
+        verify(orderSheetRepository, times(1)).save(any(OrderSheet.class));
+        verify(orderHistoryRepository, times(1)).save(any(OrderHistory.class));
+        verify(orderValidationService, times(1)).save(anyLong());
+
+    }
+
+
+}
