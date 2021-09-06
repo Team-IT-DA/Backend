58f8ab8caea90c76184ce61e568e4f047ab669c2
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-09-07 00:38:17 +0900

    [#38] feat: 컨트롤러에 배송지 입력 기능 추가

---
+++ src/main/java/com/itda/apiserver/controller/ShippingInfoController.java
@@ -0,0 +1,26 @@
+package com.itda.apiserver.controller;
+
+import com.itda.apiserver.annotation.LoginRequired;
+import com.itda.apiserver.annotation.UserId;
+import com.itda.apiserver.dto.ApiResult;
+import com.itda.apiserver.dto.ShippingInfoDto;
+import com.itda.apiserver.dto.ShippingInfoResponse;
+import com.itda.apiserver.service.ShippingInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ShippingInfoController {
+
+    private final ShippingInfoService shippingInfoService;
+
+    @LoginRequired
+    @PostMapping("/api/shippingInfo")
+    public ApiResult<ShippingInfoResponse> addShippingInfo(@UserId Long userId, @RequestBody ShippingInfoDto shippingInfoDto) {
+        Long shippingInfoId = shippingInfoService.addShippingInfo(userId, shippingInfoDto);
+        return ApiResult.ok(new ShippingInfoResponse(shippingInfoId));
+    }
+}
