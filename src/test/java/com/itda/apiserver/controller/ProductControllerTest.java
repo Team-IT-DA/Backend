e32905e79c4ca1657596d7feecbac3bb6c2dc974
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-09-14 17:00:25 +0900

    [#42] test: 리뷰 달기 테스트 추가

--- src/test/java/com/itda/apiserver/controller/ProductControllerTest.java
+++ src/test/java/com/itda/apiserver/controller/ProductControllerTest.java
@@ -2,11 +2,14 @@ package com.itda.apiserver.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itda.apiserver.domain.MainCategory;
+import com.itda.apiserver.domain.Product;
 import com.itda.apiserver.domain.Role;
 import com.itda.apiserver.domain.User;
+import com.itda.apiserver.dto.AddReviewRequestDto;
 import com.itda.apiserver.dto.AddproductRequestDto;
 import com.itda.apiserver.jwt.TokenProvider;
 import com.itda.apiserver.repository.MainCategoryRepository;
+import com.itda.apiserver.repository.ProductRepository;
 import com.itda.apiserver.repository.UserRepository;
 import com.itda.apiserver.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +23,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -28,7 +33,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -51,6 +57,9 @@ class ProductControllerTest {
 
     @MockBean
     MainCategoryRepository mainCategoryRepository;
+
+    @MockBean
+    ProductRepository productRepository;
 
     @Test
     @DisplayName("물품 추가 성공시 HTTP REQUEST 200 반환 e2e Testing")
@@ -117,7 +126,7 @@ class ProductControllerTest {
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
         when(mainCategoryRepository.findById(anyLong())).thenReturn(Optional.of(mainCategory));
 
-        for(int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             productService.addProduct(createAddProductRequestDto(), 1L);
         }
 
@@ -152,5 +161,50 @@ class ProductControllerTest {
         addproductRequestDto.setMainCategoryId(1L);
 
         return addproductRequestDto;
+    }
+
+    @Test
+    @DisplayName("제품 추가 기능 테스트")
+    void addReview() throws Exception {
+
+        AddReviewRequestDto addReviewRequest = createAddReviewRequestDto();
+        String token = "Bearer " + tokenProvider.createToken(1L);
+        Product product = Product.builder()
+                .title("맛있는 사과")
+                .price(10000)
+                .build();
+
+        User user = User.builder()
+                .name("roach")
+                .email("honux")
+                .phone("01000000000")
+                .role(Role.SELLER)
+                .password("1234@@@")
+                .account("110-440-1104123")
+                .build();
+
+        when(productRepository.findById(anyLong())).thenReturn(Optional.of(product));
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+
+        mockMvc.perform(post("/api/products/1/review")
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addReviewRequest)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    private AddReviewRequestDto createAddReviewRequestDto() {
+
+        AddReviewRequestDto addReviewRequest = new AddReviewRequestDto();
+
+        List<String> imageList = new ArrayList<>();
+        imageList.add("image url1");
+        imageList.add("image url2");
+
+        addReviewRequest.setContents("신선하고 맛있어요");
+        addReviewRequest.setImages(imageList);
+
+        return addReviewRequest;
     }
 }
--- src/test/java/com/itda/apiserver/service/ProductServiceTest.java
+++ src/test/java/com/itda/apiserver/service/ProductServiceTest.java
@@ -2,10 +2,15 @@ package com.itda.apiserver.service;
 
 import com.itda.apiserver.domain.MainCategory;
 import com.itda.apiserver.domain.Product;
+import com.itda.apiserver.domain.Review;
 import com.itda.apiserver.domain.User;
+import com.itda.apiserver.dto.AddReviewRequestDto;
 import com.itda.apiserver.dto.AddproductRequestDto;
+import com.itda.apiserver.exception.ProductNotFountException;
+import com.itda.apiserver.exception.UserNotFoundException;
 import com.itda.apiserver.repository.MainCategoryRepository;
 import com.itda.apiserver.repository.ProductRepository;
+import com.itda.apiserver.repository.ReviewRepository;
 import com.itda.apiserver.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,13 +18,10 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
-import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -37,6 +39,9 @@ class ProductServiceTest {
     @MockBean
     MainCategoryRepository mainCategoryRepository;
 
+    @MockBean
+    ReviewRepository reviewRepository;
+
     @Mock
     User user;
 
@@ -48,6 +53,9 @@ class ProductServiceTest {
 
     @Mock
     AddproductRequestDto addProductDto;
+
+    @Mock
+    AddReviewRequestDto addReviewDto;
 
     @DisplayName("판매자는 상품 추가를 할 수 있다.")
     @Test
@@ -63,4 +71,45 @@ class ProductServiceTest {
         verify(productRepository, times(1)).save(any(Product.class));
     }
 
+    @Test
+    @DisplayName("리뷰 추가 기능 테스트")
+    void addReview() {
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(productRepository.findById(anyLong())).thenReturn(Optional.of(product));
+
+        productService.addReview(1L, 1L, addReviewDto);
+
+        verify(userRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(1)).findById(anyLong());
+        verify(reviewRepository, times(1)).save(any(Review.class));
+    }
+
+    @Test
+    @DisplayName("리뷰 추가 시 존재하지 않는 유저의 id가 들어온 경우 UserNotFoundException 발생")
+    void addReviewInvalidUserId() {
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        when(productRepository.findById(anyLong())).thenReturn(Optional.of(product));
+
+        assertThrows(UserNotFoundException.class, () -> productService.addReview(1L, 1L, addReviewDto));
+
+        verify(userRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(0)).findById(anyLong());
+        verify(reviewRepository, times(0)).save(any(Review.class));
+    }
+
+    @Test
+    @DisplayName("리뷰 추가 시 존재하지 않는 제품의 id가 들어온 경우 ProductNotFoundException 발생")
+    void addReviewInvalidProductId() {
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(productRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(ProductNotFountException.class, () -> productService.addReview(1L, 1L, addReviewDto));
+
+        verify(userRepository, times(1)).findById(anyLong());
+        verify(productRepository, times(1)).findById(anyLong());
+        verify(reviewRepository, times(0)).save(any(Review.class));
+    }
 }
