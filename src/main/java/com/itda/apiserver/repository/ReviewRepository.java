36b4cf2247b536854e32182c4c16d218c47e9e12
roach <tmdgusya@naver.com>
2021-09-20 14:26:18 +0900

    feat: ReviewRepository 에서 SupportQuery 도 쓸수 있도록 추가

--- src/main/java/com/itda/apiserver/repository/ReviewRepository.java
+++ src/main/java/com/itda/apiserver/repository/ReviewRepository.java
@@ -3,5 +3,5 @@ package com.itda.apiserver.repository;
 import com.itda.apiserver.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositorySupport {
 }
