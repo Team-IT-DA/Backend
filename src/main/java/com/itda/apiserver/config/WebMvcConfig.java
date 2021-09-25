9ad5c39faf2d86a62b00ef713c15c6029368c038
roach <tmdgusya@naver.com>
2021-09-25 20:44:41 +0900

    feat: CORS

--- src/main/java/com/itda/apiserver/config/WebMvcConfig.java
+++ src/main/java/com/itda/apiserver/config/WebMvcConfig.java
@@ -46,7 +46,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 
     @Override
