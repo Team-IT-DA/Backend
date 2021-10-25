9c763f3de0a244db27f4e551c1c7f1fe1879a01e
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-10-25 16:51:36 +0900

    [#67] test: 로그인 response에 isSeller 속성 추가로 인한 테스트 수정

--- src/test/java/com/itda/apiserver/oauth/OauthControllerTest.java
+++ src/test/java/com/itda/apiserver/oauth/OauthControllerTest.java
@@ -31,7 +31,7 @@ public class OauthControllerTest {
     @DisplayName("네이버로 소셜로그인 하는 기능 테스트")
     void neverLogin() throws Exception {
 
-        when(socialLoginService.naverLogin("authorizationCode")).thenReturn(new TokenResponseDto("thisIsToken", "yeon"));
+        when(socialLoginService.naverLogin("authorizationCode")).thenReturn(new TokenResponseDto("thisIsToken", "yeon", false));
 
         mockMvc.perform(get("/api/login/naver")
                 .param("code", "authorizationCode"))
@@ -45,7 +45,7 @@ public class OauthControllerTest {
     @DisplayName("카카오로 소셜로그인 하는 기능 테스트")
     void kakaoLogin() throws Exception {
 
-        when(socialLoginService.kakaoLogin("authorizationCode")).thenReturn(new TokenResponseDto("thisIsToken", "yeon"));
+        when(socialLoginService.kakaoLogin("authorizationCode")).thenReturn(new TokenResponseDto("thisIsToken", "yeon", false));
 
         mockMvc.perform(get("/api/login/kakao")
                 .param("code", "authorizationCode"))
