60e9ebe720f048047e73bbf91e1f35d02871b4b8
roach <tmdgusya@naver.com>
2021-10-26 21:13:53 +0900

    feat: 프로덕트의 ReviewDTO 추가

--- src/main/java/com/itda/apiserver/dto/ReviewsOfProductDto.java
+++ src/main/java/com/itda/apiserver/dto/ReviewsOfProductDto.java
@@ -3,31 +3,17 @@ package com.itda.apiserver.dto;
 import com.itda.apiserver.domain.ReviewImage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class ReviewsOfProductDto {
     private Long id;
-    private Writer writer;
+    private String name;
+    private String imgURL;
     private LocalDateTime date;
-    private List<ReviewImage> images;
     private String contents;
-
-    public ReviewsOfProductDto(Long id, String name, String imgURL, LocalDateTime date, List<ReviewImage> images, String contents) {
-        this.id = id;
-        this.writer = new Writer(name, imgURL);
-        this.date = date;
-        this.images = images;
-        this.contents = contents;
-    }
-}
-
-@AllArgsConstructor
-class Writer {
-    String name;
-    String imgUrl;
+    private List<ReviewImage> reviewImages;
 }
