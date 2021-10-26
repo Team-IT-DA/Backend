ee8d91b9855ec831faeaf07c67735846c8c37a79
roach <tmdgusya@naver.com>
2021-10-26 21:14:13 +0900

    feat: 프로덕트의 리뷰를 페이징해서 볼 수 있는 인터페이스 작성

--- src/main/java/com/itda/apiserver/repository/ReviewRepository.java
+++ src/main/java/com/itda/apiserver/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.itda.apiserver.repository;
 
+import com.itda.apiserver.domain.Product;
 import com.itda.apiserver.domain.Review;
 import com.itda.apiserver.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findByUser(User user);
 
+    Long countByProduct(Product product);
+
+    List<Review> findAllByProduct(Product product, Pageable pageable);
 }
